605258e579f872b9a37a11fe2709d9bfb54786cf
paulo estevam <paulo.estevam@escolar.ifrn.edu.br>
2024-07-11 09:40:05 -0300

    lista for

---
+++ exercicios-for/5.py
@@ -0,0 +1,17 @@
+# exemplo higor questão 05 for
+#
+# inicio = int(input("digite o valor inicial: "))
+# fim = int(input("digite o valor final: "))
+# soma_primo = 0
+
+# for x in range(inicio, fim+1):
+#    numero = x
+#    divisores = 0
+#
+#    for i in range(1, numero+1):
+#        if (numero % i) == 0:
+#            divisores += 1
+#
+#    if divisores == 2:
+#        print(f"{numero} é primo")
+#        soma_primo += numero
