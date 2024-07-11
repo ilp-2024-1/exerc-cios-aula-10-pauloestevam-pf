5d519663c8e101c20f24d2e49d46e21a7dd72dc6
paulo estevam <paulo.estevam@escolar.ifrn.edu.br>
2024-07-11 09:02:46 -0300

    numprimo

---
+++ exercicios-while/ex07.py
@@ -0,0 +1,22 @@
+while True:
+    print("digite um número de 1 a 10.000 (ou '0' para encerrar):")
+    num = int(input())
+
+    if num == 0:
+        break
+
+    if num < 2:
+        print(f"o número {num} não é primo.")
+    else:
+        eprimo = True
+        for i in range(2, int{num ** 0.5} + 1):
+            if num % i == 0:
+                eprimo = False
+                break
+
+        if eprimo:
+            print(f"o número {num} é primo.")
+        else:
+            print(f"o número {num} não é primo.")
+
+print("programa encerrado.")
